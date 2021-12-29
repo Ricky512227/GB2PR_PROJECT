d285c4777a1b0e1caeacf9e8d26c350b2080c737
Ricky512227 <kamalsaidevarapalli@gmail.com>
2021-12-29 04:59:27 -0500

    Added comments and fixed naming conventions

--- WatchDog.py
+++ WatchDog.py
@@ -6,7 +6,7 @@ from CommonUtlity import executeCmd
 import datetime
 
 #Fucntion to construct the Message Notification path 
-def constructS3PathForMessage(fileType, monitoringBucketName,MonitoringBucketRegionName,objPath1,objPath2,objPath3,targetDate,tempDataSetID):
+def constructS3PathForNotificationMsg(fileType, monitoringBucketName,MonitoringBucketRegionName,objPath1,objPath2,objPath3,targetDate,tempDataSetID):
 	try:
 		#s3://gfgpr1-devl-edl2-us-east-1/prepare/cin/dflt/DSET000XXXXX/SQs STATEMACHINETRIGGER/2021-08-25/BKES/
 		s3MessagePath = "s3://"+monitoringBucketName+"/"+MonitoringBucketRegionName+"/"+objPath1+"/"+tempDataSetID+"/"+objPath2+"/"+targetDate+"/"+objPath3+"/"
@@ -16,7 +16,8 @@ def constructS3PathForMessage(fileType, monitoringBucketName,MonitoringBucketReg
 		print("Error occurred :: {0}\tLine No:: {1}".format(ex, sys.exc_info()[2].tb_lineno))
 		logger.error("Error occurred :: {0}\tLine No:: {1}".format(ex, sys.exc_info()[2].tb_lineno))
 
-def isFileExists(fileType, filePath):
+#Check if file in s3 exists or not
+def isFileExistsInS3(fileType, filePath):
 	try:
 		# -z: if the file is zero length, return 0.
 		# -f: if the path is a file, return 0.
@@ -33,7 +34,8 @@ def isFileExists(fileType, filePath):
 	except Exception as ex:
 		print("Error occurred :: {0}\tLine No:: {1}".format(ex, sys.exc_info()[2].tb_lineno))
 
-def isDirExists(fileType,objName):
+#Check if object/directory in s3 exists or not
+def isDirExistsInS3(fileType,objName):
 	try:
 		# -d: f the path is a directory, return 0.
 		# -e: if the path exists, return 0.
@@ -55,8 +57,8 @@ def isSourcFileMessageExists(fileType, objPath,messageFileName):
 	try:
 		sFilePath = os.path.join(objPath,messageFileName) 
 		logger.info("Searching for the {1} S3Object_Path :: {1}".format(fileType, objPath))
-		if isDirExists(fileType,objPath):
-			if isFileExists(fileType,sFilePath):
+		if isDirExistsInS3(fileType,objPath):
+			if isFileExistsInS3(fileType,sFilePath):
 				return True
 			else:
 				return False
@@ -65,14 +67,14 @@ def isSourcFileMessageExists(fileType, objPath,messageFileName):
 	except Exception as ex:
 		print("Error occurred :: {0}\tLine No:: {1}".format(ex, sys.exc_info()[2].tb_lineno))
 
-#First Search the Controller, S3object and Message file arraived or not.
-def searchControlMessageFile(fileType, objPath, messageFileName):
+#Search the Controller, S3object and Message file arraived or not.
+def searchControlNotificationMsgFile(fileType, objPath, messageFileName):
 	try:
 		filepath = os.path.join(objPath,messageFileName) 
 		#Checking here for  controller S3Object path.
-		if isDirExists(fileType,objPath):
+		if isDirExistsInS3(fileType,objPath):
 			#If s3Object exists then search for the messagefile.json
-			if isFileExists(fileType, filepath):
+			if isFileExistsInS3(fileType, filepath):
 				return True
 			else:
 				return False
@@ -81,8 +83,8 @@ def searchControlMessageFile(fileType, objPath, messageFileName):
 	except Exception as ex:
 		print("Error occurred :: {0}\tLine No :: {1}".format(ex, sys.exc_info()[2].tb_lineno))
 
-
-def searchSourceMessageFile(fileType, monitoringInterval,listOfSoruceMessageObjPaths,messageFileName):
+#Search the Source, S3object and Message file arraived or not.
+def searchSourceNotificationMsgFile(fileType, monitoringInterval,listOfSoruceMessageObjPaths,messageFileName):
 	try:
 		#Checking here for all sourceFiles Messages whether it exists or not
 		sourceFileCounter = 0
@@ -96,7 +98,7 @@ def searchSourceMessageFile(fileType, monitoringInterval,listOfSoruceMessageObjP
 			print (" Fetched Data and [Received Count vs Target Count] ==> {0} {1}".format(sourceFileCounter, len(listOfSoruceMessageObjPaths)))
 			print("Sleeping for {0}secs".format(monitoringInterval))
 			time.sleep(monitoringInterval)
-			searchSourceMessageFile(fileType, monitoringInterval,listOfSoruceMessageObjPaths)
+			searchSourceNotificationMsgFile(fileType, monitoringInterval,listOfSoruceMessageObjPaths)
 			return True
 		else:
 			print ( "Fetched Data and [Received Count vs Target Count] ==> {0} {1}".format(sourceFileCounter, len(listOfSoruceMessageObjPaths)))
