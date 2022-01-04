be6caf98cb159f871cd7a34c722e341cc9ccd265
Ricky512227 <kamalsaidevarapalli@gmail.com>
2022-01-04 10:08:38 +0530

    made few changes

--- feedProcessing.py
+++ feedProcessing.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
 							print ("All Messages are Arrived .... Stopping the Monitor...")
 							break
 
-				currentTimeStamp = datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+				currentTimeStamp = datetime.datetime.today().strftime('%Y-%m-%d')
 				#Setting the path  for target location for notification message files [FilesFromS3/NotificationMessages/{CurrentTimStamp}].
 				downloadedNotificationMessageFilesBasePath = os.path.join(ROOT_DIR,"FilesFromS3","NotificationMessages")
 				logger.info("downloadedNotificationMessageFilesBasePath :: {0}".format(downloadedNotificationMessageFilesBasePath))
@@ -105,9 +105,11 @@ if __name__ == '__main__':
 					failedUrlsDataSetCount = 0
 					listofS3Urls = []
 					for notificationMessageS3Obj in listOfSoruceMessageObjPaths.extend([controlMessageObjPath]):
+						messageFileName = datetime.datetime.strptime(targetDate, '%Y-%m-%d').strftime('%Y%m%d')+"*.json"
 						notificationMessageS3FileName = messageFileName
 						notificationMessageS3FilePath = os.path.join(notificationMessageS3Obj,notificationMessageS3FileName)
-						targetFielPath = downloadedNotificationMessageFilesBasePathWithCurrentDate+notificationMessageS3FileName
+						targetFielPath = downloadedNotificationMessageFilesBasePathWithCurrentDate+"/"
+						print("targetFielPath :: {0}".format(targetFielPath))
 						#Cmd to download the file to local from s3
 						cmdToGetS3FileCopyToLocal = "hdfs dfs -copyToLocal"+" "+notificationMessageS3FilePath+" "+ targetFielPath
 						returnCode, cmdData = executeCmd(cmdToGetS3FileCopyToLocal)
@@ -125,95 +127,99 @@ if __name__ == '__main__':
 							#logging the error message
 							logger.error("File {0} Failed to  Download/Copy to Local :: {1}\nReason :: {2}".format(notificationMessageS3FileName, targetFielPath, cmdData))
 
-				#Once all the s3URLs are fetched then download the controller files and source files to local.
-				if failedUrlsDataSetCount == 0:
-					failedDownloadedCount = 0 
-					#Downloading/Copying the source, controller files from s3
-					for s3Url in listofS3Urls:
-						_, tail = os.path.split(s3Url)
-						if tail.startswith("BKI"):
-							targetLocation = downloadedControlFileBasePathWithCurrentDate
-						else:
-							targetLocation = downloadedSourceFilesBasePathWithCurrentDate
-						#Copy file from s3 to local target path
-						cmdToGetS3FileCopyToLocal = "hdfs dfs -copyToLocal"+" "+s3Url+" "+targetLocation
-						returnCode, cmdData = executeCmd(cmdToGetS3FileCopyToLocal)
-						if returnCode ==0:
-							logger.info("File {0} Download/Copy to Local :: {1}".format(s3Url, targetLocation))
-						else:
-							#logging the error message
-							failedDownloadedCount = failedDownloadedCount+1
-							logger.error("File {0} Failed to  Download/Copy to Local :: {1}\nReason :: {2}".format(s3Url, targetLocation, cmdData))
-				
-				#Once all the files are downloaded then send those file to File Validations
-				if failedDownloadedCount == 0:
-					controlFilePath = glob.glob(os.path.join(downloadedControlFileBasePathWithCurrentDate,"BKI*.txt"))
-					controlFilePath = controlFilePath[0]
-					logger.info("controlFilePath :: {0}".format(controlFilePath))
-					#Open the control file and read the contents.
-					with open(controlFilePath , 'r') as file: 
-						data = file.read()
-						controlFileData =  (filter(None, data.split('\n')))
-						control_file_Dict =  convertControlDataToControlFileMap(controlFileData)
-						if len(control_file_Dict) != 0:
-							logger.info('Control File Content Collected  :: {0}'.format(control_file_Dict))
-							#Collect the downloaded ZIP file Names[Source(ASMT/DEED)]
-							SourcefileDict = {}
-							for zfile in  glob.glob(downloadedSourceFilesBasePathWithCurrentDate+"/*"):
-								if ".zip" in zfile:
-									SourcefileDict[zfile] =  int(os.stat(zfile).st_size)
-							logger.info("Source ZIP Files (Deed and ASMT Files Collected )  :: {0} , {1}".format(SourcefileDict.keys(), len(SourcefileDict.keys())))
-							if len(SourcefileDict.keys())!=0:
-								ByteValidationPassedFiles = []
-								#Compare the controlfile contents(filename and filesize) withe the source filename and filesize
-								for controlFileName, controlFileSize in control_file_Dict.items():
-									reason, absPathofSourceFile =  compareSourceandControlFile(controlFileName,controlFileSize,SourcefileDict)
-									logger.info('Received Byte_Validation reason for controlFileName :: {0} {1}'.format(controlFileName, reason))
-									if reason ==  "FileSizeMatches":
-										logger.info("CHECKING  - ControlFileData [FileName-Size] [{0} - {1}] ==> BOTH_FILEZSIZES_ARE_MATCHING with  SourceFile [FileName-Size] [{0} - {1}] ] ".format(controlFileName, controlFileSize, controlFileName, controlFileSize))
-										#STORE ALL the files Names :: Which was passed during the file size Validations, the collected file will send to be extract and get the record Count
-										ByteValidationPassedFiles.append(absPathofSourceFile)
-									
-									elif reason ==  "FileSizeNotMatches":
-										logger.info("CHECKING  - ControlFileData [FileName-Size] [{0} - {1}] ==> BOTH_FILEZSIZES_ARE_NOT_MATCHING  with  SourceFile [FileName-Size] [{2} - {3}] ]".format(controlFileName, controlFileSize, controlFileName, SourcefileDict[controlFileName]))
-
-									elif reason ==  "FileNotAvailable":
-										logger.info("CHECKING  - ControlFileData [FileName-Size] [{0} - {1}] ==> FILE_NOT_AVAILABLE in Downloaded Path :: {2} ".format(controlFileName, controlFileSize,downloadedSourceFilesBasePathWithCurrentDate))
-							
-								if len(ByteValidationPassedFiles) >0:
-									logger.info("s3- Valid ZipFiles :: {0}".format(ByteValidationPassedFiles))
-									for eachS3Zipfile in ByteValidationPassedFiles:
-										extractedFileList=  extractZipFile(eachS3Zipfile, downloadedSourceFilesBasePathWithCurrentDate)
-										if len(extractedFileList) > 0:
-											logger.info("Travesing through the unzip file and find the.gz files.. if exists then extracting that as well")
-											getGzFiles = [gzFile for gzFile in extractedFileList if ".gz" in  gzFile]
-											for eachGZfile in getGzFiles:
-												logger.info("Found and sent for extraction :: {0}".format(eachGZfile))
-												if untarGZfile(os.path.join(downloadedSourceFilesBasePathWithCurrentDate,eachGZfile)):
-													logger.info("SUCCESS Extracted the .gz file:: {0}".format(eachGZfile))
-
-													#AfterUnzip the Directory and its sub .gz Successfully then moving towards for those current extraction folder recordCount Vlaidations
-													extractedDirNameSplit = extractedFileList[0].split("_")
-													feedName = "_".join(extractedDirNameSplit[0:2])
-													feedType = extractedDirNameSplit[-1]
-													currentFeedValidationDirName = os.path.join(extractedFileList[0],feedName,feedType)
-													print("currentFeedValidationDirName :: {0}".format(currentFeedValidationDirName))
-													print("#########################################################")
-													feedRunner(currentFeedValidationDirName,downloadedSourceFilesBasePathWithCurrentDate)
-													print("#########################################################")
-
-												else:
-													logger.info(" failed Extracted the .gz file:: {0}".format(eachGZfile))
-
+					#Once all the s3URLs are fetched then download the controller files and source files to local.
+					if failedUrlsDataSetCount == 0:
+						failedDownloadedCount = 0 
+						#Downloading/Copying the source, controller files from s3
+						for s3Url in listofS3Urls:
+							_, tail = os.path.split(s3Url)
+							if tail.startswith("BKI"):
+								targetLocation = downloadedControlFileBasePathWithCurrentDate+"/"
 							else:
-								print ('No ZIP files found inside the Source Directory :: {0}'.format(downloadedSourceFilesBasePathWithCurrentDate))
-								logger.error('No ZIP files found inside the Source Directory :: {0}'.format(downloadedSourceFilesBasePathWithCurrentDate))
-						else:
-							print ('No Data found inside the Control File :: {0}'.format(controlFilePath))
-							logger.error('No Data found inside the Control File :: {0}'.format(controlFilePath))
-				else:
-					logger.error("Not all files downloaded/copied to local..")
+								targetLocation = downloadedSourceFilesBasePathWithCurrentDate+"/"
+							print ("targetLocation :: {0}".format(targetLocation))
+							#Copy file from s3 to local target path
+							cmdToGetS3FileCopyToLocal = "hdfs dfs -copyToLocal"+" "+s3Url+" "+targetLocation
+							returnCode, cmdData = executeCmd(cmdToGetS3FileCopyToLocal)
+							if returnCode ==0:
+								logger.info("File {0} Download/Copy to Local :: {1}".format(s3Url, targetLocation))
+							else:
+								#logging the error message
+								failedDownloadedCount = failedDownloadedCount+1
+								logger.error("File {0} Failed to  Download/Copy to Local :: {1}\nReason :: {2}".format(s3Url, targetLocation, cmdData))
+				
+						#Once all the files are downloaded then send those file to File Validations
+						if failedDownloadedCount == 0:
+							controlFilePath = glob.glob(os.path.join(downloadedControlFileBasePathWithCurrentDate,"BKI*.txt"))
+							controlFilePath = controlFilePath[0]
+							logger.info("controlFilePath :: {0}".format(controlFilePath))
+							#Open the control file and read the contents.
+							with open(controlFilePath , 'r') as file: 
+								data = file.read()
+								controlFileData =  (filter(None, data.split('\n')))
+								control_file_Dict =  convertControlDataToControlFileMap(controlFileData)
+								if len(control_file_Dict) != 0:
+									logger.info('Control File Content Collected  :: {0}'.format(control_file_Dict))
+									#Collect the downloaded ZIP file Names[Source(ASMT/DEED)]
+									SourcefileDict = {}
+									for zfile in  glob.glob(downloadedSourceFilesBasePathWithCurrentDate+"/*"):
+										if ".zip" in zfile:
+											SourcefileDict[zfile] =  int(os.stat(zfile).st_size)
+									logger.info("Source ZIP Files (Deed and ASMT Files Collected )  :: {0} , {1}".format(SourcefileDict.keys(), len(SourcefileDict.keys())))
+									if len(SourcefileDict.keys())!=0:
+										ByteValidationPassedFiles = []
+										#Compare the controlfile contents(filename and filesize) withe the source filename and filesize
+										for controlFileName, controlFileSize in control_file_Dict.items():
+											reason, absPathofSourceFile =  compareSourceandControlFile(controlFileName,controlFileSize,SourcefileDict)
+											logger.info('Received Byte_Validation reason for controlFileName :: {0} {1}'.format(controlFileName, reason))
+											if reason ==  "FileSizeMatches":
+												logger.info("CHECKING  - ControlFileData [FileName-Size] [{0} - {1}] ==> BOTH_FILEZSIZES_ARE_MATCHING with  SourceFile [FileName-Size] [{0} - {1}] ] ".format(controlFileName, controlFileSize, controlFileName, controlFileSize))
+												#STORE ALL the files Names :: Which was passed during the file size Validations, the collected file will send to be extract and get the record Count
+												ByteValidationPassedFiles.append(absPathofSourceFile)
+											
+											elif reason ==  "FileSizeNotMatches":
+												logger.info("CHECKING  - ControlFileData [FileName-Size] [{0} - {1}] ==> BOTH_FILEZSIZES_ARE_NOT_MATCHING  with  SourceFile [FileName-Size] [{2} - {3}] ]".format(controlFileName, controlFileSize, controlFileName, SourcefileDict[controlFileName]))
 
+											elif reason ==  "FileNotAvailable":
+												logger.info("CHECKING  - ControlFileData [FileName-Size] [{0} - {1}] ==> FILE_NOT_AVAILABLE in Downloaded Path :: {2} ".format(controlFileName, controlFileSize,downloadedSourceFilesBasePathWithCurrentDate))
+									
+										if len(ByteValidationPassedFiles) >0:
+											logger.info("s3- Valid ZipFiles :: {0}".format(ByteValidationPassedFiles))
+											for eachS3Zipfile in ByteValidationPassedFiles:
+												extractedFileList=  extractZipFile(eachS3Zipfile, downloadedSourceFilesBasePathWithCurrentDate)
+												if len(extractedFileList) > 0:
+													logger.info("Travesing through the unzip file and find the.gz files.. if exists then extracting that as well")
+													getGzFiles = [gzFile for gzFile in extractedFileList if ".gz" in  gzFile]
+													for eachGZfile in getGzFiles:
+														logger.info("Found and sent for extraction :: {0}".format(eachGZfile))
+														if untarGZfile(os.path.join(downloadedSourceFilesBasePathWithCurrentDate,eachGZfile)):
+															logger.info("SUCCESS Extracted the .gz file:: {0}".format(eachGZfile))
+
+															#AfterUnzip the Directory and its sub .gz Successfully then moving towards for those current extraction folder recordCount Vlaidations
+															extractedDirNameSplit = extractedFileList[0].split("_")
+															feedName = "_".join(extractedDirNameSplit[0:2])
+															feedType = extractedDirNameSplit[-1]
+															currentFeedValidationDirName = os.path.join(extractedFileList[0],feedName,feedType)
+															print("currentFeedValidationDirName :: {0}".format(currentFeedValidationDirName))
+															print("#########################################################")
+															feedRunner(currentFeedValidationDirName,downloadedSourceFilesBasePathWithCurrentDate)
+															print("#########################################################")
+														else:
+															print("Failed Extracted the .gz file:: {0}".format(eachGZfile))
+															logger.info("Failed Extracted the .gz file:: {0}".format(eachGZfile))
+
+									else:
+										print ('No ZIP files found inside the Source Directory :: {0}'.format(downloadedSourceFilesBasePathWithCurrentDate))
+										logger.error('No ZIP files found inside the Source Directory :: {0}'.format(downloadedSourceFilesBasePathWithCurrentDate))
+								else:
+									print ('No Data found inside the Control File :: {0}'.format(controlFilePath))
+									logger.error('No Data found inside the Control File :: {0}'.format(controlFilePath))
+						else:
+							print("Not all files downloaded/copied to local..")
+							logger.error("Not all files downloaded/copied to local..")
+					else:
+						print("Failed to collect the Notification Messages ..")
+						logger.error("Failed to collect the Notification Messages ..")
 			else:
 				print("InValid Date Format received :: {0}\n It should be [YYYY-MM-DD]".format(targetDate))
 		else:
