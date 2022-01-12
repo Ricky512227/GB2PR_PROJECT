8b39a4103539f70c8a0a3a698272beea48b5b335
Ricky512227 <kamalsaidevarapalli@gmail.com>
2022-01-12 23:08:20 +0530

    Update latestfeedprocessing.py

--- latestfeedprocessing.py
+++ latestfeedprocessing.py
@@ -113,7 +113,6 @@ if __name__ == '__main__':
         #         tmps3Prefix = "/".join(s3prefix.split("/")[:-1])
         #         listOfSoruceMessageObjPaths = [constructS3PathForNotificationMsg(sourceType,monitoringBucketName,tmps3Prefix)]
 
-
         # Download the files based on the inputs through s3_bucket and s3_prefixes
         failedDownloadedCount = 0
         for s3Url in listofS3Urls:
@@ -137,79 +136,79 @@ if __name__ == '__main__':
                 logger.error("File {0} Failed to  Download/Copy to Local :: {1}\nReason :: {2}".format(s3Url, targetLocation, cmdData))
 
 
-            #Once all the files are downloaded then send those file to File Validations
-            if failedDownloadedCount == 0:
-                controlFilePath = glob.glob(os.path.join(downloadedControlFileBasePathWithCurrentDate,"BKI*.txt"))
-                controlFilePath = controlFilePath[0]
-                logger.info("controlFilePath :: {0}".format(controlFilePath))
-                #Open the control file and read the contents.
-                with open(controlFilePath , 'r') as file:
-                    data = file.read()
-                    controlFileData =  (filter(None, data.split('\n')))
-                    control_file_Dict =  convertControlDataToControlFileMap(controlFileData)
-                    if len(control_file_Dict) != 0:
-                        logger.info('Control File Content Collected  :: {0}'.format(control_file_Dict))
-                        #Collect the downloaded ZIP file Names[Source(ASMT/DEED)]
-                        SourcefileDict = {}
-                        for zfile in  glob.glob(downloadedSourceFilesBasePathWithCurrentDate+"/*"):
-                            logger.info('SOURCE_FILE = {}'.format(zfile))
-                            if ".zip.001" in zfile:
-                                SourcefileDict[zfile] =  int(os.stat(zfile).st_size)
-                        logger.info("Source ZIP Files (Deed and ASMT Files Collected )  :: {0} , {1}".format(SourcefileDict.keys(), len(SourcefileDict.keys())))
-                        if len(SourcefileDict.keys())!=0:
-                            ByteValidationPassedFiles = []
-                            #Compare the controlfile contents(filename and filesize) withe the source filename and filesize
-                            for controlFileName, controlFileSize in control_file_Dict.items():
-                                reason, absPathofSourceFile =  compareSourceandControlFile(controlFileName,controlFileSize,SourcefileDict)
-                                logger.info('Received Byte_Validation reason for controlFileName :: {0} {1}'.format(controlFileName, reason))
-                                if reason ==  "FileSizeMatches":
-                                    logger.info("CHECKING  - ControlFileData [FileName-Size] [{0} - {1}] ==> BOTH_FILEZSIZES_ARE_MATCHING with  SourceFile [FileName-Size] [{0} - {1}] ] ".format(controlFileName, controlFileSize, controlFileName, controlFileSize))
-                                    #STORE ALL the files Names :: Which was passed during the file size Validations, the collected file will send to be extract and get the record Count
-                                    ByteValidationPassedFiles.append(absPathofSourceFile)
+        #Once all the files are downloaded then send those file to File Validations
+        if failedDownloadedCount == 0:
+            controlFilePath = glob.glob(os.path.join(downloadedControlFileBasePathWithCurrentDate,"BKI*.txt"))
+            controlFilePath = controlFilePath[0]
+            logger.info("controlFilePath :: {0}".format(controlFilePath))
+            #Open the control file and read the contents.
+            with open(controlFilePath , 'r') as file:
+                data = file.read()
+                controlFileData =  (filter(None, data.split('\n')))
+                control_file_Dict =  convertControlDataToControlFileMap(controlFileData)
+                if len(control_file_Dict) != 0:
+                    logger.info('Control File Content Collected  :: {0}'.format(control_file_Dict))
+                    #Collect the downloaded ZIP file Names[Source(ASMT/DEED)]
+                    SourcefileDict = {}
+                    for zfile in  glob.glob(downloadedSourceFilesBasePathWithCurrentDate+"/*"):
+                        logger.info('SOURCE_FILE = {}'.format(zfile))
+                        if ".zip.001" in zfile:
+                            SourcefileDict[zfile] =  int(os.stat(zfile).st_size)
+                    logger.info("Source ZIP Files (Deed and ASMT Files Collected )  :: {0} , {1}".format(SourcefileDict.keys(), len(SourcefileDict.keys())))
+                    if len(SourcefileDict.keys())!=0:
+                        ByteValidationPassedFiles = []
+                        #Compare the controlfile contents(filename and filesize) withe the source filename and filesize
+                        for controlFileName, controlFileSize in control_file_Dict.items():
+                            reason, absPathofSourceFile =  compareSourceandControlFile(controlFileName,controlFileSize,SourcefileDict)
+                            logger.info('Received Byte_Validation reason for controlFileName :: {0} {1}'.format(controlFileName, reason))
+                            if reason ==  "FileSizeMatches":
+                                logger.info("CHECKING  - ControlFileData [FileName-Size] [{0} - {1}] ==> BOTH_FILEZSIZES_ARE_MATCHING with  SourceFile [FileName-Size] [{0} - {1}] ] ".format(controlFileName, controlFileSize, controlFileName, controlFileSize))
+                                #STORE ALL the files Names :: Which was passed during the file size Validations, the collected file will send to be extract and get the record Count
+                                ByteValidationPassedFiles.append(absPathofSourceFile)
 
-                                elif reason ==  "FileSizeNotMatches":
-                                    logger.info("CHECKING  - ControlFileData [FileName-Size] [{0} - {1}] ==> BOTH_FILEZSIZES_ARE_NOT_MATCHING  with  SourceFile [FileName-Size] [{2} - {3}] ]".format(controlFileName, controlFileSize, controlFileName, SourcefileDict[controlFileName]))
+                            elif reason ==  "FileSizeNotMatches":
+                                logger.info("CHECKING  - ControlFileData [FileName-Size] [{0} - {1}] ==> BOTH_FILEZSIZES_ARE_NOT_MATCHING  with  SourceFile [FileName-Size] [{2} - {3}] ]".format(controlFileName, controlFileSize, controlFileName, SourcefileDict[controlFileName]))
 
-                                elif reason ==  "FileNotAvailable":
-                                    logger.info("CHECKING  - ControlFileData [FileName-Size] [{0} - {1}] ==> FILE_NOT_AVAILABLE in Downloaded Path :: {2} ".format(controlFileName, controlFileSize,downloadedSourceFilesBasePathWithCurrentDate))
+                            elif reason ==  "FileNotAvailable":
+                                logger.info("CHECKING  - ControlFileData [FileName-Size] [{0} - {1}] ==> FILE_NOT_AVAILABLE in Downloaded Path :: {2} ".format(controlFileName, controlFileSize,downloadedSourceFilesBasePathWithCurrentDate))
 
-                            if len(ByteValidationPassedFiles) >0:
-                                logger.info("s3- Valid ZipFiles :: {0}".format(ByteValidationPassedFiles))
-                                for eachS3Zipfile in ByteValidationPassedFiles:
-                                    extractedFileList=  extractZipFile(eachS3Zipfile, downloadedSourceFilesBasePathWithCurrentDate)
-                                    if len(extractedFileList) > 0:
-                                        logger.info("Travesing through the unzip file and find the.gz files.. if exists then extracting that as well")
-                                        getGzFiles = [gzFile for gzFile in extractedFileList if ".gz" in  gzFile]
-                                        for eachGZfile in getGzFiles:
-                                            logger.info("Found and sent for extraction :: {0}".format(eachGZfile))
-                                            if untarGZfile(os.path.join(downloadedSourceFilesBasePathWithCurrentDate,eachGZfile)):
-                                                logger.info("SUCCESS Extracted the .gz file:: {0}".format(eachGZfile))
+                        if len(ByteValidationPassedFiles) >0:
+                            logger.info("s3- Valid ZipFiles :: {0}".format(ByteValidationPassedFiles))
+                            for eachS3Zipfile in ByteValidationPassedFiles:
+                                extractedFileList=  extractZipFile(eachS3Zipfile, downloadedSourceFilesBasePathWithCurrentDate)
+                                if len(extractedFileList) > 0:
+                                    logger.info("Travesing through the unzip file and find the.gz files.. if exists then extracting that as well")
+                                    getGzFiles = [gzFile for gzFile in extractedFileList if ".gz" in  gzFile]
+                                    for eachGZfile in getGzFiles:
+                                        logger.info("Found and sent for extraction :: {0}".format(eachGZfile))
+                                        if untarGZfile(os.path.join(downloadedSourceFilesBasePathWithCurrentDate,eachGZfile)):
+                                            logger.info("SUCCESS Extracted the .gz file:: {0}".format(eachGZfile))
 
-                                                #AfterUnzip the Directory and its sub .gz Successfully then moving towards for those current extraction folder recordCount Vlaidations
-                                                extractedDirNameSplit = extractedFileList[0].split("_")
-                                                feedName = "_".join(extractedDirNameSplit[0:2])
-                                                feedType = extractedDirNameSplit[-1]
-                                                currentFeedValidationDirName = os.path.join(extractedFileList[0],feedName,feedType)
-                                                print("currentFeedValidationDirName :: {0}".format(currentFeedValidationDirName))
-                                                print("#########################################################")
-                                                feedRunner(currentFeedValidationDirName,downloadedSourceFilesBasePathWithCurrentDate)
-                                                print("#########################################################")
-                                            else:
-                                                print("Failed Extracted the .gz file:: {0}".format(eachGZfile))
-                                                logger.info("Failed Extracted the .gz file:: {0}".format(eachGZfile))
+                                            #AfterUnzip the Directory and its sub .gz Successfully then moving towards for those current extraction folder recordCount Vlaidations
+                                            extractedDirNameSplit = extractedFileList[0].split("_")
+                                            feedName = "_".join(extractedDirNameSplit[0:2])
+                                            feedType = extractedDirNameSplit[-1]
+                                            currentFeedValidationDirName = os.path.join(extractedFileList[0],feedName,feedType)
+                                            print("currentFeedValidationDirName :: {0}".format(currentFeedValidationDirName))
+                                            print("#########################################################")
+                                            feedRunner(currentFeedValidationDirName,downloadedSourceFilesBasePathWithCurrentDate)
+                                            print("#########################################################")
+                                        else:
+                                            print("Failed Extracted the .gz file:: {0}".format(eachGZfile))
+                                            logger.info("Failed Extracted the .gz file:: {0}".format(eachGZfile))
 
-                        else:
-                            print ('No ZIP files found inside the Source Directory :: {0}'.format(downloadedSourceFilesBasePathWithCurrentDate))
-                            logger.error('No ZIP files found inside the Source Directory :: {0}'.format(downloadedSourceFilesBasePathWithCurrentDate))
                     else:
-                        print ('No Data found inside the Control File :: {0}'.format(controlFilePath))
-                        logger.error('No Data found inside the Control File :: {0}'.format(controlFilePath))
-            else:
-                print("Not all files downloaded/copied to local..")
-                logger.error("Not all files downloaded/copied to local..")
+                        print ('No ZIP files found inside the Source Directory :: {0}'.format(downloadedSourceFilesBasePathWithCurrentDate))
+                        logger.error('No ZIP files found inside the Source Directory :: {0}'.format(downloadedSourceFilesBasePathWithCurrentDate))
+                else:
+                    print ('No Data found inside the Control File :: {0}'.format(controlFilePath))
+                    logger.error('No Data found inside the Control File :: {0}'.format(controlFilePath))
         else:
-            print("Failed to collect the Notification Messages ..")
-            logger.error("Failed to collect the Notification Messages ..")
+            print("Not all files downloaded/copied to local..")
+            logger.error("Not all files downloaded/copied to local..")
+    # else:
+    #     print("Failed to collect the Notification Messages ..")
+    #     logger.error("Failed to collect the Notification Messages ..")
     except Exception as ex:
         print("Error occurred :: {0}\tLine No:: {1}".format(ex, sys.exc_info()[2].tb_lineno))
         logger.error("Error occurred :: {0}\tLine No:: {1}".format(ex, sys.exc_info()[2].tb_lineno))
